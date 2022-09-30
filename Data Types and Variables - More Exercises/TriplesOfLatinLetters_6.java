6858de937de2c4f0d40fdeb7d866741646c33a28
Dayana Uzunova <daqnauzunova@abv.bg>
2022-09-30 12:17:34 +0300

    Add some more exercises.
    Adding different exercises.

---
+++ Data Types and Variables - More Exercises/TriplesOfLatinLetters_6.java
@@ -0,0 +1,11 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class TriplesOfLatinLetters_6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}

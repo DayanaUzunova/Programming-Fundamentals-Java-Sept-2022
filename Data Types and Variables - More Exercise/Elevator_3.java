df268fc892766bfcfbdcabee2c4eee4cd2207112
Dayana Uzunova <daqnauzunova@abv.bg>
2022-09-30 12:09:32 +0300

    Add some more exercise.
    Different exercises with data types and variables.

---
+++ Data Types and Variables - More Exercise/Elevator_3.java
@@ -0,0 +1,10 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class Elevator_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}

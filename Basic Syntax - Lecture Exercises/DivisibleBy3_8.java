f018985eb50ed02dc8e9be6363fc59446f92b483
Dayana Uzunova <daqnauzunova@abv.bg>
2022-11-13 19:31:15 +0200

    Add exercises.
    Add exercises from lecture Basic Syntax.

---
+++ Basic Syntax - Lecture Exercises/DivisibleBy3_8.java
@@ -0,0 +1,13 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class DivisibleBy3_8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 3; i <= 100; i+=3) {
+            System.out.println(i);
+        }
+    }
+}
